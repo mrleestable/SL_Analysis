267a1651f47b1c5092fb56b4baf5b76201ff4918
mrleestable <98743557+mrleestable@users.noreply.github.com>
2025-06-29 14:22:03 +0900

    Update SL_Analysis.m

--- SL_Analysis.m
+++ SL_Analysis.m
@@ -10,3 +10,17 @@ xmlPath = fullfile('decompressed_slx', 'simulink', 'stateflow.xml');
 xDoc = xmlread(xmlPath);
 
 
+
+
+info = Simulink.MDLInfo('model.slx');
+disp(info.BlockDiagramName);
+disp(info.Description);
+
+load_system('model');
+blocks = find_system('model','Type','Block');
+for i = 1:length(blocks)
+  blockName = get_param(blocks{i},'Name');
+  blockType = get_param(blocks{i},'BlockType');
+  parent = get_param(blocks{i},'Parent');
+  disp([blockName,' : ', blockType, ' (Parent : ',parent,')']);
+end
